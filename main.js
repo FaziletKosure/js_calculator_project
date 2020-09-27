0342f0a4903f724c373711cb40c335b33d6f6288
FaziletKosure <faziletkosure1@gmail.com>
2020-09-27 18:15:09 +0200

    add calculater

---
+++ main.js
@@ -0,0 +1,68 @@
+// function c(val) {
+//     // if ('*-/+'.includes(val)){}
+//     document.getElementById("edu").value +=val;
+  
+// }
+// function equals() {
+//     console.log('equals');
+//     let num=document.getElementById("edu").value;
+//     let evalNum=eval(num);
+//     document.getElementById("edu").value=evalNum;
+    
+// }
+// function clearit() {
+//     console.log('clr');
+//     document.getElementById("edu").value="";
+// }
+
+var clickedButton, equationDone = false;
+window.onload = function(){    
+    $('button').click(function(){        
+        clickedButton = $(this).attr('id');  
+        click();
+    });    
+}
+
+function click(){
+    if(clickedButton === 'clear')
+        document.getElementById('inputField').innerHTML = '0';
+    else if (clickedButton === 'divi' || clickedButton === 'multi' || clickedButton === 'sub' || clickedButton === 'dot' || clickedButton === 'add'){ 
+        if(document.getElementById('inputField').innerHTML === 'Error')
+            clear();
+        document.getElementById('inputField').innerHTML += document.getElementById(clickedButton).innerHTML;
+        equationDone = false;
+    }    
+    else if (clickedButton === 'equal'){        
+        var equation = document.getElementById('inputField').innerHTML;        
+        var answer;
+        
+        try {
+            eval(equation); 
+            answer = eval(equation);
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                document.getElementById('inputField').innerHTML = 'Error';
+            }
+        }       
+        
+        if(answer == 'Infinity')
+            document.getElementById('inputField').innerHTML = 'Error';  
+        else if(Number.isInteger(answer))//if it's an int don't round to two
+            document.getElementById('inputField').innerHTML = answer;               
+        else
+            document.getElementById('inputField').innerHTML = answer.toFixed(2);   
+        
+        equationDone = true;
+    }
+    else{       
+        clear();
+        document.getElementById('inputField').innerHTML += document.getElementById(clickedButton).innerHTML;        
+    }        
+}
+
+function clear(){    
+    if(document.getElementById('inputField').innerHTML === '0' || equationDone === true || document.getElementById('inputField').innerHTML === 'Error')  
+        document.getElementById('inputField').innerHTML = '';
+    
+    equationDone = false;
+}
